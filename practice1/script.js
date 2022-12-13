522821e84cb32c6962817396c5781c2baae435bc
ChilloutGeek <adamgemenez@gmail.com>
2022-12-13 09:21:46 +0800

    added calculatev1

--- practice1/script.js
+++ practice1/script.js
@@ -1,5 +1,6 @@
+
 $(document).ready(function(){
-    
+      
     $('div.band').hide();
     $('div.picture.nothree').show();
 
@@ -8,5 +9,26 @@ $(document).ready(function(){
         $('div.band, img.picture').hide();
         $('div.band.no' + bvalue).show();
         $('img.picture.no' + bvalue).show();
-    })
-})
+    });
+
+    function Calculate(first, second, multiplier){
+        resistor = (first+second)*multiplier
+        return resistor         
+    };
+
+    $('#calculate').click(function(){
+
+        var first = $('#first-band').val();
+
+        var second = $('#second-band').val();
+
+        var multiplier = $('#multiplier').val();
+
+        testband = Calculate(first,second,multiplier);
+
+        $('#resistorvalue').text(testband);
+
+        console.log(testband);
+    });
+
+});
