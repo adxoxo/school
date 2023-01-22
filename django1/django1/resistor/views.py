96b4055f4d5561ac585d97553b11bbbd4702dd2f
ChilloutGeek <adamgemenez@gmail.com>
2023-01-23 05:17:42 +0800

    add blocks

--- django1/django1/resistor/views.py
+++ django1/django1/resistor/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def main(request):
 
-    return render(request, 'resistor/base.html', {})
+    return render(request, 'resistor.html', {})
